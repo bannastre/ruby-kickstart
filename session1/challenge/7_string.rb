65719cbbea1d7dec1f8c782779fa7b8beb67f82f
bannastre <bannastre@gmail.com>
2017-04-15 12:57:15 +0100

    Session 1 Challenge 7 passed

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,14 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  letters = ""
+  string.length.times do |l|
+    next if string[l].to_s.downcase != 'r'
+      letters << string[l.next].to_s
+    end
+  return letters
 end
