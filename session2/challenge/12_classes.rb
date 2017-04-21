651230dbd42d19f1655769133a7a6c680aaf21a9
bannastre <bannastre@gmail.com>
2017-04-21 21:06:15 +0100

    Session 2 Challenge 12 passed

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -20,9 +20,61 @@
 # f.to_s               # => "50/100"
 # f.to_f               # => 0.5
 
+# Define a class called Fraction
+# This class is used to represent a ratio of two integers
+# Include setter methods called numerator and denominator that allow the user to change these values
+# Also include a method called to_f that returns the ratio as a float (a number with a decimal point, such as 10.3)
+# Also include a method called lowest, which returns a new Fraction, where the numerator and denominator
+# are reduced to lowest terms (ie 20/60 becomes 1/3)
+# This will require finding the greatest common divisor for the numerator and denominator
+# then dividing both by that number.
+# I have included a greatest common divisor method for you
+# You should also define a method, to_s, that will represent the Fraction as a String
+#
+# EXAMPLE:
+# f = Fraction.new 20, 60
+# f.numerator          # => 20
+# f.denominator        # => 60
+# f.to_s               # => "20/60"
+# f.lowest.to_s        # => "1/3"
+# f.numerator = 50
+# f.denominator = 100
+# f.to_s               # => "50/100"
+# f.to_f               # => 0.5
+
 class Fraction
-  def gcd(a,b)
-    return a if b == 0
-    gcd(b, a%b)
+  def initialize(a,b)
+    @numerator = a
+    @denominator = b
+  end
+
+  attr_accessor 'numerator', 'denominator'
+
+  def numerator
+    @numerator
+  end
+
+  def denominator
+    @denominator
+  end
+
+  def gcd(numerator, denominator)
+    return numerator if denominator == 0
+    gcd(denominator, numerator%denominator)
+  end
+
+  def to_f
+    return numerator.to_f / denominator.to_f
+  end
+
+  def to_s
+    return numerator.to_s + '/' + denominator.to_s
+  end
+
+  def lowest
+    top = (numerator.to_i / gcd(numerator, denominator)).to_s
+    bottom = (denominator.to_i / gcd(numerator, denominator)).to_s
+    lowest_fraction = top + '/' + bottom
+    return lowest_fraction
   end
 end
